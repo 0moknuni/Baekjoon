3d4cfde60f597fa3d68b108c2f13ef9fcc0df64b
0moknuni <114228828+0moknuni@users.noreply.github.com>
2025-02-09 02:52:28 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//두 수의 합을 구하는 프로그램
+int main()
+{
+	int n;		//반복문 트리거
+	int a, b;	//입력받은 수를 저장할 변수
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+	}
+
+	return 0;
+}
