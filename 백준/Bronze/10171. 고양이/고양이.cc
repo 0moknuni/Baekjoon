a6f7e09f88147aa86bb33e7e0c6a514770eb5219
0moknuni <114228828+0moknuni@users.noreply.github.com>
2025-02-09 02:50:11 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//고양이 문양 프로그램
+int main()
+{
+	printf("\\    /\\\n");	//\를 출력을 위해 \\을 연속으로 써주어야 함.
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|");
+
+	return 0;
+}
