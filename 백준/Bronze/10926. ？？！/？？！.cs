c1858d49eeec696336e839328feb10012e74718f
0moknuni <114228828+0moknuni@users.noreply.github.com>
2025-02-09 02:51:40 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준풀이c샵
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string id = "";
+            id = Console.ReadLine();
+            Console.WriteLine(id + "??!");
+        }
+    }
+}
