6adadad96e5571d1008cd512f7edbf7d7a26a3c9
0moknuni <114228828+0moknuni@users.noreply.github.com>
2025-02-09 02:50:18 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//개 문양 프로그램
+int main()
+{
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");	//"도 출력하려면 \"로 해야함!
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|");
+
+	return 0;
+}
