d72945ab5e2f7e8f5b2e8e451913370cad547a00
0moknuni <114228828+0moknuni@users.noreply.github.com>
2025-02-09 02:54:27 +0900

    [Bronze V] Title: 직사각형, Time: 60 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27323
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(width * height);
+        }
+    }
+}
