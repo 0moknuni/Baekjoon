fd111b82bf8904e12df22701f3282031e2a9b241
0moknuni <114228828+0moknuni@users.noreply.github.com>
2025-02-09 02:53:22 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18108
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 0;
+
+            year = int.Parse(Console.ReadLine());
+            Console.WriteLine(year - 543);
+        }
+    }
+}
