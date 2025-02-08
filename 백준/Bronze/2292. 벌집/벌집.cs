6b35d7674a5a84b7fb2f3f0fd48e0df043e58bef
0moknuni <114228828+0moknuni@users.noreply.github.com>
2025-02-09 02:33:16 +0900

    [Bronze II] Title: 벌집, Time: 60 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace _2292
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindRoom(int.Parse(Console.ReadLine())));
+        }
+
+        static int FindRoom(int num)
+        {
+            int i = 0;
+            int roomNum = 1;
+            do
+            {
+                roomNum += 6 * i;
+                i++;
+            } while (roomNum < num);
+            
+            return i;
+        }
+    }
+}
