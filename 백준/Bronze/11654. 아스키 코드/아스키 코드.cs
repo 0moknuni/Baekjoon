851b059d3fa981953254678a0aed567346e4cb19
0moknuni <114228828+0moknuni@users.noreply.github.com>
2025-02-09 02:52:42 +0900

    [Bronze V] Title: 아스키 코드, Time: 60 ms, Memory: 5444 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace _11654
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int asciiCode;
+
+            asciiCode = Console.Read(); //read()시 아스크 코드를 반환
+            Console.WriteLine(asciiCode);
+        }
+    }
+}
