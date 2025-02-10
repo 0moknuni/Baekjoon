58c0add2cede1ba75380a5b3fd91c038cb22ff41
0moknuni <114228828+0moknuni@users.noreply.github.com>
2025-02-10 16:37:47 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 372 ms, Memory: 30556 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cs
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -10,12 +11,13 @@ namespace _11650
         static void Main(string[] args)
         {
             //input
-            int count = int.Parse(Console.ReadLine());
+            var reader = new StreamReader(Console.OpenStandardInput());
+            int count = int.Parse(reader.ReadLine());
             List<List<int>> xy = new List<List<int>>();
 
             for (int i = 0; i < count; i++)
             {
-                var nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                var nm = Array.ConvertAll(reader.ReadLine().Split(), int.Parse);
                 xy.Add(nm.ToList()); 
             }
 
